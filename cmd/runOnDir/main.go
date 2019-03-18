9b78bfaef688daed26bbd451f02a7b687dffb35d
TehSphinX <tehsphinx@web.de>
2019-03-18 23:24:48 +0100

    cmd to run on entire directory of solutions

---
+++ cmd/runOnDir/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path"
+
+	"github.com/exercism/go-analyzer/analyzer"
+)
+
+var (
+	exercise  = flag.String("exercise", "", "exercise slug (e.g. 'two-fer'). All sub-dirs must contain this exercise!")
+	parentDir = flag.String("parentDir", "", "run analyzer for all sub-directories in this folder")
+	output    = flag.String("output", "analysis.json", "name of the output file")
+)
+
+func main() {
+	flag.Parse()
+	if *exercise == "" || *parentDir == "" || *output == "" {
+		log.Println("invalid empty parameter")
+		flag.Usage()
+		return
+	}
+
+	dirs, err := analyzer.GetDirs(".", http.Dir(*parentDir))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, dir := range dirs {
+		res := analyzer.Analyze(*exercise, path.Join(*parentDir, dir))
+		for _, err := range res.Errors {
+			log.Println(err)
+		}
+		if len(res.Errors) != 0 {
+			continue
+		}
+
+		bytes, err := json.MarshalIndent(res, "", "\t")
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if err := ioutil.WriteFile(path.Join(*parentDir, dir, *output), append(bytes, '\n'), 0644); err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+}
